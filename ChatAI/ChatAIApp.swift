9708c1dd9c86dc733ad2c317ab5c61fa543c2a8d
qwotic <qwoticreator@gmail.com>
2023-01-20 19:53:29 +0700

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Данил Белов on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
